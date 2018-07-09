c0f006768eec1cbc14381b19ef6402b40fb8deb2
Javier <javier.h.poncell@gmail.com>
2018-07-09 13:31:23 +0200

    File path fixing.

--- src/main.py
+++ src/main.py
@@ -15,10 +15,10 @@ photos_captured = 0
 
 @app.route('/')
 def make_photo():
-	camera.capture('src/img/image.jpg')
+	camera.capture('../src/img/image.jpg')
 	#photos_captured = photos_captured + 1
 	#print '>>Captured Photo N° '+ str(photos_captured)
-	return send_from_directory('src/img', 'image.jpg')
+	return send_from_directory('../src/img', 'image.jpg')
 
 @app.route('/test')
 def test_render():
