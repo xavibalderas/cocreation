d13dd9035369728f6c744aefde1bc52e18c72091
Javier <javier.h.poncell@gmail.com>
2018-07-09 16:01:51 +0200

    Comments added.

--- src/main.py
+++ src/main.py
@@ -1,36 +1,47 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, send_from_directory, send_file
+
+from flask import Flask, render_template, send_file
 import time
-from datetime import datetime
 from picamera import PiCamera
 
 
 app = Flask(__name__)
 
+# Let's load the camera. We're using the V2 PiCamera:
+# Accepted resolutions for maximum FoV: 
+# 3280x2464 (4:3)
+# 1640x1232 (4:3)
+# 1640x922  (16:9)
+# 1280x720  (16:9)
+# 640x480   (4:3)
 camera = PiCamera()
 camera.resolution = (3280,2464)
 camera.start_preview()
 time.sleep(5)
 
+# At GET, we make a photo and then we display it.
 @app.route('/')
 def make_photo():
 	camera.capture('image.jpg')
 	print '>>Captured Photo'
-	#return send_from_directory('/src/img', 'image.jpg')
+	# Use 'as_attachment=True' as another parameter of send_file() if you want to serve the download in the browser
 	return send_file('/usr/src/app/image.jpg')
 
+# Testing view
 @app.route('/test')
 def test_render():
 	return render_template('test.html')
-
+# 404 handler
 @app.errorhandler(404)
 def page_not_found(error):
+	print error
 	return render_template('page_not_found.html')
-
+# 500 handler
 @app.errorhandler(500)
 def internal_error(error):
 	print error
 	return render_template('internal_error.html')
+
 
 if __name__ == '__main__':
 
