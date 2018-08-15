dba33072dbd6641e2ba75b632e745970b1af8aee
Starbuck65 <javier.h.poncell@gmail.com>
2018-08-15 13:57:43 +0100

    PDF final styling, handling for multiple arguments in  url query.

--- src/main.py
+++ src/main.py
@@ -6,6 +6,18 @@ import cups
 from picamera import PiCamera
 from fpdf import FPDF
 
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import landscape
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib.colors import Color,black,white
+from reportlab.platypus import Paragraph
+from reportlab.lib.styles import ParagraphStyle, getSampleStyleSheet
+from reportlab.lib.enums import TA_CENTER
+from reportlab.platypus import BaseDocTemplate, Paragraph, PageTemplate, Frame
+from reportlab.pdfbase.pdfmetrics import stringWidth
+
 
 app = Flask(__name__)
 
@@ -39,39 +51,51 @@ def print_pdf():
 	
 	if request.method == 'GET':
 		# First we snap a picture
-		camera.capture('image.jpg',resize=(640,480))
-		# We gather the material name from the url arguments
-		nameMaterial = request.args.get('data',default = '', type = str)
+		camera.capture('image.jpg',resize=(1280,720))
+		# We gather the names of the materials from the url arguments
+		num_args = len(request.args)
+		materials = []
+		for i in range(num_args):
+			materials.append(request.args.get('value' + str(i+1),default = '', type = str))
+				
+		################################################################
+		# Start PDF with ReporLab module
+		# Prepare fonts
+		pdfmetrics.registerFont(TTFont('Verdana','verdana.ttf'))
+		# Static components
+		def draw_static(canvas,doc):
+			canvas.saveState()
+			canvas.setFont('Verdana',8)
+			canvas.drawImage('image.jpg',0,0,600,400)
+			canvas.setFillColor(white)
+			canvas.rect(28,18,80,12,fill=True,stroke=False)	
+			canvas.setFillColor(black)
+			canvas.drawString(30,20,'IKEA Spreitenbach')
+			canvas.restoreState()
+		# Creating the document, adding components
+		doc = BaseDocTemplate('material.pdf',pagesize=(7*inch,5*inch))
+		# Adding frames
+		frames = []
+		positions = [330,313,296,279,262,245,228,211,194,177]
+		for i in range(num_args):
+			textWidth = stringWidth(materials[i],'Verdana',14)
+			frames.append(Frame(22, positions[i],(textWidth+20),(18),bottomPadding = 0,topPadding=0, id='material'+str(i+1),showBoundary=0))
 		
-		# Start PDF 
-		pdf = FPDF('L','in',(5,7))
-		pdf.add_font('verdana','', fname='/usr/share/fonts/TTF/verdana.ttf',uni=True)
-		
-		print '>>DEBUG: Pdf created.'
+		template = PageTemplate(id='test',frames=frames,onPage=draw_static)
+		doc.addPageTemplates([template])
+		# Paragraph styling
+		styles = getSampleStyleSheet()
+		styles.add(ParagraphStyle(name='header',fontName='Verdana',fontSize=14,
+								  alignment=TA_CENTER,leading=18,backColor=white))
+		# Adding paragraphs
+		components = []
+		for i in range(num_args):
+			components.append(Paragraph(materials[i], styles['header']))
 
-		pdf.add_page()
-
-		pdf.image('image.jpg',w=2,h=2)
-		pdf.set_font('Verdana',size=14)
-		pdf.cell(w=1,h=1,txt=nameMaterial)
-		
-		pdf.output('test.pdf','F')
-		print '>>DEBUG: End reach.'
-		
-		# Get printer and print PDF
-		cups_handler = cups.Connection()
-		printers = cups_handler.getPrinters()
-		print '>>DEBUG: Searching printers...'
-		for printer in printers:
-			print printer, printers[printer]["device-uri"]
-		
-		print '>>DEBUG: Sending pdf to first printer.'
-		printer_id=printers.keys()[0]
-		cups_handler.printFile(printer_id,'test.pdf',"Test",{})
-		print '>>DEBUG: File sent.'
-	
+		# Assembling document and storing it.
+		doc.build(components)
 		# Serve PDF	
-		return send_file('test.pdf')
+		return send_file('material.pdf')
 		
 	return render_template('page_not_found.html')
 
@@ -81,27 +105,50 @@ def download_pdf():
 	if request.method == 'GET':
 		# First we snap a picture
 		camera.capture('image.jpg',resize=(1280,720))
-		# We gather the material name from the url arguments
-		nameMaterial = request.args.get('data',default = '', type = str)
+		# We gather the names of the materials from the url arguments
+		num_args = len(request.args)
+		materials = []
+		for i in range(num_args):
+			materials.append(request.args.get('value' + str(i+1),default = '', type = str))
+				
+		################################################################
+		# Start PDF with ReporLab module
+		# Prepare fonts
+		pdfmetrics.registerFont(TTFont('Verdana','verdana.ttf'))
+		# Static components
+		def draw_static(canvas,doc):
+			canvas.saveState()
+			canvas.setFont('Verdana',8)
+			canvas.drawImage('image.jpg',0,0,600,400)
+			canvas.setFillColor(white)
+			canvas.rect(28,18,80,12,fill=True,stroke=False)	
+			canvas.setFillColor(black)
+			canvas.drawString(30,20,'IKEA Spreitenbach')
+			canvas.restoreState()
+		# Creating the document, adding components
+		doc = BaseDocTemplate('material.pdf',pagesize=(7*inch,5*inch))
+		# Adding frames
+		frames = []
+		positions = [330,313,296,279,262,245,228,211,194,177]
+		for i in range(num_args):
+			textWidth = stringWidth(materials[i],'Verdana',14)
+			frames.append(Frame(22, positions[i],(textWidth+20),(18),bottomPadding = 0,topPadding=0, id='material'+str(i+1),showBoundary=0))
 		
-		# Start PDF 
-		pdf = FPDF('L','in',(5,7))
-		pdf.add_font('verdana','', fname='/usr/share/fonts/TTF/verdana.ttf',uni=True)
-		pdf.set_auto_page_break('false',margin=0.0);
-		print '>>DEBUG: Pdf created.'
-		pdf.add_page()
+		template = PageTemplate(id='test',frames=frames,onPage=draw_static)
+		doc.addPageTemplates([template])
+		# Paragraph styling
+		styles = getSampleStyleSheet()
+		styles.add(ParagraphStyle(name='header',fontName='Verdana',fontSize=14,
+								  alignment=TA_CENTER,leading=18,backColor=white))
+		# Adding paragraphs
+		components = []
+		for i in range(num_args):
+			components.append(Paragraph(materials[i], styles['header']))
 
-
-		pdf.image('image.jpg',w=4,h=4)
-		pdf.set_font('Verdana',size = 14)
-		pdf.cell(4,1,txt=nameMaterial,fill='True')
-
-
-		pdf.output('test.pdf','F')
-		print '>>DEBUG: End reach.'
-	
+		# Assembling document and storing it.
+		doc.build(components)
 		# Serve PDF	
-		return send_file('test.pdf')
+		return send_file('material.pdf')
 		
 	return render_template('page_not_found.html')
 
