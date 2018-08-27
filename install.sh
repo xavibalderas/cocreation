b15da435065ddf0e53982d59451e6f0b7578cd63
Starbuck65 <javier.h.poncell@gmail.com>
2018-08-27 07:38:28 +0100

    Install script with dependencies

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-mkdir /home/pi/apps
-cd apps
-git clone https://github.com/xavibalderas/cocreation.git
-cd cocreation
 sudo apt install libcups2-dev ttf-mscorefonts-installer
 sudo pip install pycups reportlab
 echo Installed!
